518d2b88f9dc3035d842ff5b42c53f0f8711c80d
Hai Long <nguyenhailong.works@gmail.com>
2024-06-21 19:59:21 +0900

    Thêm API INSERT USER

--- xcloset_api/app/Http/Controllers/UsersController.php
+++ xcloset_api/app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Validator;
 
 class UsersController extends Controller
 {
@@ -18,6 +19,54 @@ class UsersController extends Controller
     }
 
     //[POST] Insert thông tin người dùng
+    public function upload(Request $request){
+        //$validator giúp kiểm tra giá trị đầu vào
+        //xem có phù hợp không
+        $validator = Validator::make($request->all(),
+        [
+            'role' =>'required',//yêu cầu đầu vào, tức là không được null
+            'name' =>'required',//không được null
+            'email' =>'required|email',//không được null và phải có cấu trúc email
+            'username' =>'required',//không được null
+            'password' =>'required',//không được null
 
 
+
+        ]);
+
+        if($validator->fails()){
+            $data=[
+                'status'=>422,
+                'message'=>$validator->messages()
+
+
+            ];
+
+            return response()->json($data,422);
+
+        }else{
+        //Insert Data vào Model
+            //Chuẩn bị dữ liệu để Insert vào Model
+            $users = new User;//User chính là tên model
+            $users->role=$request->role;
+            $users->name=$request->name;
+            $users->email=$request->email;
+            $users->username=$request->username;
+            $users->password=$request->password;
+
+            //Lưu(insert) Data và Model
+            $users->save();
+
+            $data=[
+                'status'=>200,
+                'message'=>'Data Uploaded Successfully :))',
+            ];
+
+            return response()->json($data,200);
+
+
+
+        }
+
+    }
 }
